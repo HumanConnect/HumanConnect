2fa0e4f6d68f53d6c661fa488ea4ca024e8a0022
lstarrsmith <logan29smith@gmail.com>
2015-02-05 15:40:30 -0500

    added create on userscontroller and have html displaying user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 		render(:index)
 	end
 
+	def create
+        new_user = User.create({email: params["email"], password: params["password"]})
+    end
+
 	def test
 
 		##formatting the sessionTokenObject to send to Human API
